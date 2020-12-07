0365c9068d6a646560c5f5f34bc76aa74af11517
Maksim <mel2112@mail.ru>
2020-12-08 02:17:01 +0300

    Add animation

--- app/src/main/java/com/example/note/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/note/ui/AddNoteFragment.kt
@@ -2,27 +2,21 @@ package com.example.note.ui
 
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.lifecycle.whenStarted
 import androidx.navigation.fragment.findNavController
-import androidx.room.Room
 import com.example.note.App
 import com.example.note.R
-import com.example.note.database.AppDatabase
 import com.example.note.database.Category
 import com.example.note.database.Note
 import kotlinx.android.synthetic.main.fragment_add_note.*
-import kotlinx.android.synthetic.main.fragment_notes.*
-import kotlinx.android.synthetic.main.item_note.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 
 class AddNoteFragment : Fragment(), CoroutineScope {
@@ -36,7 +30,7 @@ class AddNoteFragment : Fragment(), CoroutineScope {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         launch {
-            val categories = withContext(Dispatchers.IO) {App.db.categoryDao().getAll()}
+            val categories = withContext(Dispatchers.IO) { App.db.categoryDao().getAll() }
 
             inputCategoryNote.setAdapter(
                     ArrayAdapter(
@@ -45,11 +39,10 @@ class AddNoteFragment : Fragment(), CoroutineScope {
                             categories.map { it.name }
                     )
             )
-            
+
             inputCategoryNote.setOnFocusChangeListener { _, hasFocus ->
                 if (hasFocus) inputCategoryNote.showDropDown()
             }
-
         }
 
         btnAddNote.setOnClickListener {
@@ -57,25 +50,75 @@ class AddNoteFragment : Fragment(), CoroutineScope {
             val body = inputTextNote.text.toString()
             val category = inputCategoryNote.text.toString()
 
-            if (title.isNotEmpty() && body.isNotEmpty() && category.isNotEmpty())
+            when {
+                title.isEmpty() -> {
+                    AnimatorSet().apply {
+                        playTogether(
+                                ObjectAnimator.ofFloat(inputTitleNote, "translationX", 1f, 10f, 1f),
+                                ObjectAnimator.ofArgb(
+                                            inputTitleNote,
+                                            "hintColor",
+                                            resources.getColor(R.color.black),
+                                            resources.getColor(R.color.red),
+                                            resources.getColor(R.color.black)
+                                    ).apply {
+                                        addUpdateListener {
+                                            val animatedValue = it.animatedValue as Int
+                                            inputTitleNote.backgroundTintList = ColorStateList.valueOf(animatedValue)
+                                        }
+                                    }
+                        )
+                        start()
+                    }
+                }
 
-                launch {
+                body.isEmpty() -> {
+                    AnimatorSet().apply {
+                        playTogether(
+                                ObjectAnimator.ofFloat(inputTextNote, "translationX", 1f, 10f, 1f),
+                                ObjectAnimator.ofArgb(
+                                        inputTextNote,
+                                        "hintColor",
+                                        resources.getColor(R.color.black),
+                                        resources.getColor(R.color.red),
+                                        resources.getColor(R.color.black)
+                                ).apply {
+                                    addUpdateListener {
+                                        val animatedValue = it.animatedValue as Int
+                                        inputTextNote.backgroundTintList = ColorStateList.valueOf(animatedValue)
+                                    }
+                                }
+                        )
+                        start()
+                    }
+                }
+
+                category.isEmpty() -> {
+                    AnimatorSet().apply {
+                        playTogether(
+                                ObjectAnimator.ofFloat(inputCategoryNote, "translationX", 1f, 10f, 1f),
+                                ObjectAnimator.ofArgb(
+                                        inputCategoryNote,
+                                        "hintColor",
+                                        resources.getColor(R.color.black),
+                                        resources.getColor(R.color.red),
+                                        resources.getColor(R.color.black)
+                                ).apply {
+                                    addUpdateListener {
+                                        val animatedValue = it.animatedValue as Int
+                                        inputCategoryNote.backgroundTintList = ColorStateList.valueOf(animatedValue)
+                                    }
+                                }
+                        )
+                        start()
+                    }
+                }
+
+                else -> launch {
                     withContext(Dispatchers.IO) { App.db.categoryDao().insert(Category(category)) }
                     withContext(Dispatchers.IO) { App.db.noteDao().insert(Note(title, body, category)) }
                     findNavController().popBackStack()
                 }
-            else
-            (AnimatorInflater.loadAnimator(requireContext(), R.animator.error_animation) as AnimatorSet).apply {
-                setTarget(inputTitleNote)
-                start()
-            }
-            (AnimatorInflater.loadAnimator(requireContext(), R.animator.error_animation) as AnimatorSet).apply {
-                setTarget(inputTextNote)
-                start()
-            }
-            (AnimatorInflater.loadAnimator(requireContext(), R.animator.error_animation) as AnimatorSet).apply {
-                setTarget(inputCategoryNote)
-                start()
             }
         }
     }
